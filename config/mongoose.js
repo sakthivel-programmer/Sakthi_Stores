7b9c0ae7119c1cc40189bcef950ec81073519944
sakthivel-programmer <120034628+sakthivel-programmer@users.noreply.github.com>
2023-11-11 11:07:56 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,34 @@
-const mongoose=require("mongoose");
+// const mongoose=require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1/Sakthi_Stores");
+// mongoose.connect("mongodb://127.0.0.1/Sakthi_Stores");
+
+// const db = mongoose.connection;
+
+// db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+// db.once('open', function(){
+//     console.log('Connected to Database :: MongoDB');
+// });
+
+// module.exports = db;
+
+const mongoose = require('mongoose');
+//fill your database name here
+
+mongoose.connect(
+  'mongodb+srv://klsakthi333:Mongoose@cluster0.@Sakthi333.mongodb.net/review_managment_system_db?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+db.on('error', console.error.bind('console', 'error'));
 
-db.once('open', function(){
-    console.log('Connected to Database :: MongoDB');
+db.once('open', function () {
+  console.log('welcome connect to database');
 });
 
 module.exports = db;
