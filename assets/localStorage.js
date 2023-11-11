a136340e3a93eed4f4bba602738f404be9df06ee
sakthivel-programmer <120034628+sakthivel-programmer@users.noreply.github.com>
2023-11-11 19:33:24 +0530

    Update localStorage.js

--- assets/localStorage.js
+++ assets/localStorage.js
@@ -1,9 +1,9 @@
-const _sessionStorage = {}
+// const _sessionStorage = {}
 
-const sessionStorage = {
-  setItem: (key, value) => _sessionStorage[key] = value,
-  getItem: (key) => _sessionStorage[key],
-  removeItem: (key) => delete _sessionStorage[key],
-}
+// const sessionStorage = {
+//   setItem: (key, value) => _sessionStorage[key] = value,
+//   getItem: (key) => _sessionStorage[key],
+//   removeItem: (key) => delete _sessionStorage[key],
+// }
 
-module.exports = sessionStorage
+// module.exports = sessionStorage
