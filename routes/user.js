f4617cd02e5c48fb72660577e890a1b92282c007
sakthivel-programmer <120034628+sakthivel-programmer@users.noreply.github.com>
2024-01-11 15:58:37 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -8,12 +8,17 @@ const Controller=require("../controllers/index");
 router.get("/sign_up",Controller.signUp);
 // router for create user in the DB
 router.post("/createUser",Controller.createUser);
-// router for create user sess  ion 
+// router for create user session 
 router.post("/createSession",Controller.createSession);
+router.get("/createSession",Controller.createSession);
 // ṛouter for buy now option
 router.get("/buyNow",Controller.buyNow);
+router.get("/add_to_cart",Controller.addToCart)
+router.get("/open_cart",Controller.openCart)
+router.get("/remove_from_cart/:item_id",Controller.removeFromCart)
+router.post('/searching', Controller.searchingg);
 // router for search option
-router.post("/search",Controller.search);
+// router.post("/search",Controller.search);
 router.post("/detailed_view",Controller.detailedView);
 // make the router available
 module.exports=router;
